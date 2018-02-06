27800ad1797f11a6a7be6ef6ca7daa7762342d6b
Reyna Egson <ro.egson@gmail.com>
2018-02-06 16:53:05 -0600

    version1

---
+++ app.js
@@ -0,0 +1,122 @@
+
+
+let pizzaPrice = document.querySelector('.price'); //view price in span tag
+let viewTotal = document.querySelector('#view');  //button you press to view your total price/list 
+let btn = document.querySelectorAll('.add'); //Button you press to add order(four buttons total ie cheese, pepperoni, etc)
+
+
+
+  //event Listeners
+
+   
+   //values
+   	for(var i = 0; i < btn.length; i++) {
+   	   btn[i].addEventListener('click', function(e){
+       var x = 0;
+       var size = document.getElementById('size').value;
+       const crust = document.getElementById('crust').value;
+       const quantity = document.getElementById('quantity').value;
+  
+	  //Instantiate pizza
+
+         const cheese = new Pizza(size, crust, quantity, total);
+        
+        
+       
+     //Instantiate UI
+        const ui = new UI();
+      
+
+      //add to order
+       ui.addPizzaToOrder(cheese);
+
+
+   	    e.preventDefault();
+
+   	})}
+  
+
+
+
+//Pizza Object Constructor
+
+function Pizza(size, crust, quantity, total) {
+	this.size = size
+	this.crust = crust;
+	this.quantity = quantity;
+	this.total = quantity * 12;
+ 
+   
+ }
+  
+
+
+
+
+//UI constructor
+ 
+ function UI() {}
+
+
+ UI.prototype.addPizzaToOrder = function(cheese){
+       const list = document.getElementById('orderList');
+     //create li
+     const content = document.createElement('ul');
+     content.innerHTML = `
+       
+       <li>${cheese.size}</li>
+       <li>${cheese.crust}</li>
+       <li> Number of pizzas:${cheese.quantity}</li>
+       <li> Total Price:<i class="fas fa-dollar-sign"></i>${cheese.total}</li>
+
+      `
+      pizzaCart.appendChild(content);
+      console.log(pizzaCart);
+
+
+     
+
+ }
+
+
+
+
+
+
+
+
+//Modal Pop up
+var modal = document.getElementById('simpleModal');
+var modalBtn = document.getElementById('submitInfo');
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+var checkout = document.getElementById('submitInfo');
+var pizzaCart = document.getElementById('PizzaCart');
+
+
+//listen for click
+
+viewTotal.addEventListener('click', openModal);
+
+closeBtn.addEventListener('click', closeModal);
+modalBtn.addEventListener('click', closeModal);
+
+//window object click
+window.addEventListener('click', outsideClick);
+
+//function open
+
+function openModal() {
+	modal.style.display = 'block';
+}
+
+//function close
+
+function closeModal() {
+	modal.style.display = 'none';
+}
+
+function outsideClick(e) {
+	if(e.target == modal){
+	modal.style.display = 'none';
+}}
+
